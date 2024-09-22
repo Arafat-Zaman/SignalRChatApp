a48dc3753d335f45106092c5627e704d33bf7117
Arafat Zaman Anik <arafat.zaman.anik@gmail.com>
2024-09-22 23:25:20 +0600

    Add project files.

---
+++ SignalRChatApp/Models/ChatHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+public class ChatHub : Hub
+{
+    // Send a message from the client to all connected clients
+    public async Task SendMessage(string user, string message)
+    {
+        await Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
+}
